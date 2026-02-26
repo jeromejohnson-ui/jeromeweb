0158b4d2b631e6f54cb1ee767635acdfecd36822
jeromejohnson-ui <jeromejohnson.abc.123@gmail.com>
2026-02-26 12:37:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const SUPABASE_URL = "https://goupceptsijattqsqxma.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvdXBjZXB0c2lqYXR0cXNxeG1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMTEwOTMsImV4cCI6MjA4NzU4NzA5M30.g816kRFiFBdeDmz65JRBO6R4dMghF6VffaYbEX0Kb8M";
 
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
+const supabase = window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
 
 const form = document.getElementById("feedbackForm");
 const response = document.getElementById("response");
@@ -39,4 +36,5 @@ form.addEventListener("submit", async function (event) {
     }
 
 });
+
 
